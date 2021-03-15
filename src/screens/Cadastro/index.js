b8d0b978ed8f16876d9d57dc02ed6c679cec22b4
oFhelipe <fhelipe.rodrigues@fatec.sp.gov.br>
2021-03-15 18:29:21 -0300

    Criando tela de cadastro e fazendo alguns ajustes

---
+++ src/screens/Cadastro/index.js
@@ -0,0 +1,31 @@
+import LoginCadTemplate from '../../components/LoginCadTemplate'
+import InputLoginCad from '../../components/InputLoginCad'
+import Button from '../../components/Button'
+import { Link } from 'react-router-dom'
+import logo from "../../assets/images/logo-small.svg";
+import './styles.scss'
+
+function Cadastro () {
+  return (
+    <LoginCadTemplate reverse>
+      <form id="cadastro-form">
+        <img src={logo} alt="logo" />
+        <h1>Criar conta</h1>
+        <InputLoginCad type="text" placeholder="Usuário" required />
+        <InputLoginCad type="email" placeholder="E-mail" required/>
+        <InputLoginCad type="email" placeholder="E-mail" required/>
+        <div id="password-container">
+        <InputLoginCad type="password" placeholder="Senha" required/>
+        <InputLoginCad type="password" placeholder="Confirmar senha" required/>
+        </div>
+        <Button>Cadastrar</Button>
+        <div id="criar-conta-container">
+          <p>Já tem uma conta?</p>
+          <Link to="/login">Faça login!</Link>
+        </div>
+      </form>
+    </LoginCadTemplate>
+  );
+}
+
+export default Cadastro;
