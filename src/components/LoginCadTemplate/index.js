a5dc586396270dc5f09acd35c0eb6d76afb65c14
oFhelipe <fhelipe.rodrigues@fatec.sp.gov.br>
2021-03-15 14:37:21 -0300

    Refazendo estilo LoginCad usando mobile first

--- src/components/LoginCadTemplate/index.js
+++ src/components/LoginCadTemplate/index.js
@@ -3,11 +3,11 @@ import logo from '../../assets/images/logo-full.svg'
 
 function LoginCadTemplate ({children, reverse}) {
   return (
-    <div id="login-cad-container" className={reverse ? 'reversed-login-cad-container' : ''}>
-      <div id="quadrado-branco">
+    <div className={`login-cad-container${reverse ? ' reversed-login-cad-container' : ''}`}>
+      <div className="quadrado-branco">
         {children}
       </div>
-      <div id="quadrado-escuro">
+      <div className="quadrado-escuro">
         <p>Cadastre-se no GameStore e descubra os melhores jogos para jogar.</p>
         <img src={logo} alt="logo" />
         <p>Criar uma conta, é gratuito e facil de usar.</p>
