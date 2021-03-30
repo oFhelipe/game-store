739148b5cfa3b68214009b8669cd0c3f4fa5e7bd
“wdwf” <w_dwf@hotmail.com>
2021-03-30 13:48:28 -0300

    Construção do menu mobile e desktop

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,15 @@
-import LancamentosBox from '../../components/LancamentosBox'
-import Footer from '../../components/Footer'
+// import LancamentosBox from '../../components/LancamentosBox';
+// import Footer from '../../components/Footer';
 import './styles.scss'
+
+import Menu from '../../components/Menu';
 
 function Home () {
   return (
     <div id="home-container" className="app-container">
-      <LancamentosBox />
-      <Footer />
+      <Menu />
+      {/* <LancamentosBox /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
