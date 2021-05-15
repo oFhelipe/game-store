eafe880598af977aedea0ce45e5d0484c16f2a89
wdwf <w_dwf@hotmail.com>
2021-05-14 22:45:56 -0300

    conexão com a api e criando login e cadastro

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -1,25 +1,33 @@
 import { useState } from "react";
-import LoginCadTemplate from '../../components/LoginCadTemplate'
-import InputLoginCad from '../../components/InputLoginCad'
-import Button from '../../components/Button'
-import { Link } from 'react-router-dom'
+import LoginCadTemplate from '../../components/LoginCadTemplate';
+import InputLoginCad from '../../components/InputLoginCad';
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 import logo from "../../assets/images/logo-small.svg";
-import './styles.scss'
+import api from '../../services/api';
+import './styles.scss';
 
 function Cadastro ({ history }) {
-  const [ userName, setUserName ] = useState(null);
+  const [ username, setUsername ] = useState(null);
+  const [ name, setName ] = useState(null);
+  const [ lastname, setLastname ] = useState(null);
+  const [ birthday, setBirthday ] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword,setConfirmPassword] = useState(null);
   const [email ,setEmail] = useState(null);
   const [confirmEmail ,setConfirmEmail] = useState(null);
-  function handleRegister(e) {
+  
+  async function handleRegister(e) {
     e.preventDefault();
-    if (!userName || !password || !confirmPassword || !email || !confirmEmail) {
-      setUserName('');
+    if (!username || !password || !confirmPassword || !email || !confirmEmail || !name || !lastname || !birthday) {
+      setUsername('');
       setPassword('');
       setConfirmPassword('');
       setEmail('');
       setConfirmEmail('');
+      setName('');
+      setLastname('');
+      setBirthday('');
       window.alert('Insira todos os dados do cadastro');
       return;
     }
@@ -31,18 +39,48 @@ function Cadastro ({ history }) {
       window.alert('Os emails não coincidem');
       return;
     }
-    window.alert('Usuário cadastrado com sucesso')
-    history.push('/login')
+
+    const fullName = `${name} ${lastname}`
+
+    
+    try {
+      const response = await api.post('/user', {
+        name:fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+        birthday
+      })
+      //mostrar modal de usuario cadastrado com sucesso
+
+      //esperar alguns segundos para redirecionar para o login
+      //localStorage.setItem('user', JSON.stringify([...response]))
+      history.push('/login')
+
+    } catch (error) {
+      window.alert(error.response.data.message);
+    }
+    
   }
+    
+
+  
 
   return (
     <LoginCadTemplate reverse>
       <form id="cadastro-form">
         <img src={logo} alt="logo" />
         <h1>Criar conta</h1>
-        <InputLoginCad className={userName === ''&& 'required-input'} onChange={(e) => { setUserName(e.currentTarget.value) }} type="text" placeholder="Usuário" required />
+        <div id="password-container">
+        <InputLoginCad className={name === ''&& 'required-input'} onChange={(e) => { setName(e.currentTarget.value) }} type="text" placeholder="Nome" required/>
+        <InputLoginCad className={lastname === ''&& 'required-input'} onChange={(e) => { setLastname(e.currentTarget.value) }} type="text" placeholder="sobrenome" required/>
+        </div>
+        <InputLoginCad className={username === ''&& 'required-input'} onChange={(e) => { setUsername(e.currentTarget.value) }} type="text" placeholder="Usuário" required />
         <InputLoginCad className={email === ''&& 'required-input'} onChange={(e) => { setEmail(e.currentTarget.value) }} type="email" placeholder="E-mail" required/>
         <InputLoginCad className={confirmEmail === ''&& 'required-input'} onChange={(e) => { setConfirmEmail(e.currentTarget.value) }} type="email" placeholder="Confirmar E-mail" required/>
+        <InputLoginCad className={birthday === ''&& 'required-input'} onChange={(e) => { setBirthday(e.currentTarget.value) }} type="date" required/>
+        
         <div id="password-container">
         <InputLoginCad className={password === ''&& 'required-input'} onChange={(e) => { setPassword(e.currentTarget.value) }} type="password" placeholder="Senha" required/>
         <InputLoginCad className={confirmPassword === ''&& 'required-input'} onChange={(e) => { setConfirmPassword(e.currentTarget.value) }} type="password" placeholder="Confirmar senha" required/>
